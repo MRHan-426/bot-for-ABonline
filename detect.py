3a64ebe877cc1dc46c698ed7eb0691cf7b23f5cb
MRHan-426 <1207140677@qq.com>
2023-09-19 00:28:56 -0400

    purchase bot

---
+++ detect.py
@@ -0,0 +1,116 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+from paddleocr import PaddleOCR, draw_ocr
+from PIL import Image
+
+ocr = PaddleOCR(use_angle_cls=True, lang="en")  # need to run only once to download and load model into memory
+print("ocr initialization success")
+BUTTON_BUY = cv2.imread('image/button_buy.jpg', cv2.IMREAD_GRAYSCALE)
+MARKET_REGION = (338, 315, 900, 700)
+PRICE_REGION = (920, 473, 120, 531)
+BUY_COMFIRMATION_POS = (1008, 860) # 954~1050  850~880 
+MINUS_SIGN_POS = (651, 708) # range 5
+
+class autoBuy():
+    def __init__(self, parent=None):
+        """!
+        @brief      Constructs a new instance.
+
+        """
+        self.screen = np.array(pyautogui.screenshot(region = MARKET_REGION))
+        self.price_region = np.array(pyautogui.screenshot(region = PRICE_REGION))
+        self.gray_screen = cv2.cvtColor(self.screen, cv2.COLOR_BGR2GRAY)
+
+    def find_buy_button(self):
+        """!
+        @brief      Use opencv template match to locate "BUY" button.
+
+        """
+        h, w = BUTTON_BUY.shape
+        result = cv2.matchTemplate(self.gray_screen, BUTTON_BUY, cv2.TM_CCOEFF_NORMED)
+        loc = np.where(result >= 0.8)
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(self.screen, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 2)
+        cv2.imwrite("image/screen2.jpg", self.screen)
+
+
+    def update_screenshot(self):
+        """!
+        @brief      Update screenshot.
+
+        """
+        self.screen = np.array(pyautogui.screenshot(region = MARKET_REGION))
+        self.price_region = np.array(pyautogui.screenshot(region = PRICE_REGION))
+        self.gray_screen = cv2.cvtColor(self.screen, cv2.COLOR_BGR2GRAY)    
+        
+
+    def read_price_from_screen(self):
+        """!
+        @brief      Use paddle ocr to get the prices.
+
+        """
+        result = ocr.ocr(self.price_region, cls=True)
+        for idx in range(len(result)):
+            res = result[idx]
+            if res != None:
+                for line in res:
+                    print(line)
+
+        result = result[0]
+        boxes = [line[0] for line in result]
+        txts = [line[1][0] for line in result]
+        scores = [line[1][1] for line in result]
+        im_show = draw_ocr(self.price_region, boxes, txts, scores, font_path='./fonts/simfang.ttf')
+        im_show = Image.fromarray(im_show)
+        return np.array(im_show)
+
+        # cv2.destroyAllWindows()
+        # im_show.save('image/result.jpg')
+        # cv2.imwrite("image/screen2.jpg", self.price_region)
+
+
+    def mouse_move(self, pos1, pos2, duration=0.5, num_points = 10):
+        """!
+        @brief      Mouse move from position1 to position2
+                    Simulate human mouse movement
+        
+        @param      duration: moving time
+        @param      num_points: interval points between the two points
+        """
+
+        x1, y1 = pos1
+        x2, y2 = pos2
+        sleep_duration = duration / num_points
+
+        for _ in range(num_points):
+            x_offset = random.randint(-5, 5)
+            y_offset = random.randint(-5, 5)
+            x1 += (x2 - x1) / num_points + x_offset
+            y1 += (y2 - y1) / num_points + y_offset
+            pyautogui.moveTo(x1, y1, duration = sleep_duration, tween = pyautogui.easeInOutQuad)
+            
+        pyautogui.moveTo(x2, y2)
+
+    def mouse_click(self):
+        """!
+        @brief      Mouse click once
+
+        """
+        pyautogui.click()
+
+
+if __name__ == '__main__':
+
+    aB = autoBuy()
+    while(True):
+        # aB.find_buy_button()
+        aB.update_screenshot()
+        img = aB.read_price_from_screen()
+        cv2.imshow("Price_OCR", img)
+        cv2.waitKey(1000)
+    cv2.destroyAllWindows()
+
+
+
